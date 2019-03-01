9146db80ff58c40b5c7986b501c09b5bfec7d7bd
Sylwester <sylwestronowak@gmail.com>
2019-03-01 11:55:16 +0100

    end

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 //Mapa z nowymi inwestycjami
-var map = L.map('interactive_map').setView([52.208404, 21.539050], 10);
+var map = L.map('interactive_map').setView([52.222920, 21.230119], 9);
 L.tileLayer('https://api.mapbox.com/styles/v1/progress39/cjsn6p6xc1i3u1fp5ojm4wcc0/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicHJvZ3Jlc3MzOSIsImEiOiJjanNuNmRqbmYwODVpNDNueXdhemVhM2poIn0.NCNzuoyp2M57xHtTrUSRKw', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
 }).addTo(map);
-
 
 //Znaczniki miejsc
 var mainIcon = L.icon({
@@ -12,6 +11,7 @@ var mainIcon = L.icon({
        iconSize: [50, 50], // size of the icon
        });
 
+//Każdy znacznik posiada własny adres
 var planes = [
   ["Warszawska 141, Siedlce",52.166940, 22.283313],
   ["Osiedle pod Klonami, Łomianki",52.332970, 20.889077],
@@ -21,6 +21,7 @@ var planes = [
 	["Osiedle pod Kasztanami, Wołomin",52.346211, 21.228740]
 	];
 
+//Dodanie funkcji wyświetlania okienka "popup" po naciśnięciu na daną inwestycję na mapie
 for (var i = 0; i < planes.length; i++) {
     	marker = new L.marker([planes[i][1],planes[i][2]], {icon: mainIcon})
     	.bindPopup(planes[i][0])
@@ -31,3 +32,14 @@ for (var i = 0; i < planes.length; i++) {
 if (map.scrollWheelZoom) {
   map.scrollWheelZoom.disable();
 }
+
+//Płynne przejścia do sekcji
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
